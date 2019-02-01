9dea25106321b4836cdbb8b40424501497e802f4
codewiz <c.chaiwat.ch@gmail.com>
2019-02-01 23:05:53 +0700

    Add Structure, login page.

--- noter/__init__.py
+++ noter/__init__.py
@@ -1,20 +1,19 @@
 import os
-
 from flask import Flask, render_template, url_for, views
-#from flask_login import LoginManager
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-#lm = LoginManager()
+lm = LoginManager()
 
-basedir = os.path.abspath(os.path.basedir(__file__))
+basedir = os.path.abspath(os.path.realpath(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(basedir, "noter.db"))
 app.config['SECRET_KEY'] = 'this-is-my-secret'
 db = SQLAlchemy(app)
 
-#lm.init_app(app)
-#lm.session_protection = "strong"
-#lm.login_view = "login"
+lm.init_app(app)
+lm.session_protection = "strong"
+lm.login_view = "login"
 
-#from .views import *
+from .views import *
